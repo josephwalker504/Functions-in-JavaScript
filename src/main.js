9b6b39b07d793025bece183c94d173afb614150a
joseph walker <josephwalker504@gmail.com>
2019-10-11 13:47:31 -0500

    Added code from the ChickenMonkey Practice

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+//     // Divide the current number by 2, and check if the remainder is 0
+//     if (currentNumber % 2 === 0) {
+//         console.log(currentNumber) // Only 2, 4, 6 will appear
+//     }
+// }
+
+for (let chickenMonkeyGame = 1; chickenMonkeyGame <= 100; chickenMonkeyGame++){
+    if (chickenMonkeyGame % 5 === 0) {
+        console.log("Chicken")
+    } else if (chickenMonkeyGame % 7 === 0){ 
+        console.log("Monkey")
+    } else if (chickenMonkeyGame % 15 === 0) {
+        console.log("ChickenMonkey")
+    }
+}
